b3b291f7ceab8b0436cbefc7f7c0fc128cb129be
mohvi <167302439+connectionmz@users.noreply.github.com>
2025-01-23 00:47:46 +0000

    Destaque rotaa
    Destaque rotaa

--- src/components/desktop/HeaderDesk.js
+++ src/components/desktop/HeaderDesk.js
@@ -1,42 +1,38 @@
 import React, { useState } from "react";
-import SearchIcon from "@mui/icons-material/Search";
-import StoreMallDirectoryIcon from "@mui/icons-material/StoreMallDirectory";
-import BusinessIcon from "@mui/icons-material/Business";
-import GroupIcon from "@mui/icons-material/Group";
-import DescriptionIcon from "@mui/icons-material/Description";
-import ChatIcon from "@mui/icons-material/Chat";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import DomainIcon from "@mui/icons-material/Domain";
-import GavelIcon from "@mui/icons-material/Gavel";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import {
   AppBar,
   Box,
   IconButton,
-  InputBase,
   Toolbar,
   Typography,
   useMediaQuery,
   Button,
 } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import StoreMallDirectoryIcon from "@mui/icons-material/StoreMallDirectory";
+import GavelIcon from "@mui/icons-material/Gavel";
+import DomainIcon from "@mui/icons-material/Domain";
+import DescriptionIcon from "@mui/icons-material/Description";
+import ChatIcon from "@mui/icons-material/Chat";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { logo } from "../../utils/utils";
 
 const HeaderDesk = ({ user }) => {
-  const navigate = useNavigate(); 
-  const publicPanel = user?.publicPainel; 
-  const [searchTerm, setSearchTerm] = useState(""); 
-  const isMobile = useMediaQuery("(max-width:600px)"); 
+  const navigate = useNavigate();
+  const location = useLocation(); // Rota atual
+  const publicPanel = user?.publicPainel;
+  const isMobile = useMediaQuery("(max-width:600px)");
 
   const navItems = [
     { to: "/search", icon: <SearchIcon fontSize="large" />, label: "Pesquisar" },
     { to: "/stores", icon: <StoreMallDirectoryIcon fontSize="large" />, label: "Lojas" },
     { to: "/concursos", icon: <GavelIcon fontSize="large" />, label: "Concursos" },
-    { to: "/explore", icon: <DomainIcon  fontSize="large" />, label: "Empresas" },
+    { to: "/explore", icon: <DomainIcon fontSize="large" />, label: "Empresas" },
     { to: "/cotacoes", icon: <DescriptionIcon fontSize="large" />, label: "Cotações" },
     { to: "/inbox", icon: <ChatIcon fontSize="large" />, label: "Mensagens" },
     { to: "/app", icon: <AccountCircleIcon fontSize="large" />, label: "Perfil do Usuário" },
   ];
-
 
   return (
     <AppBar position="sticky" sx={{ backgroundColor: "#fff", boxShadow: 3 }}>
@@ -50,22 +46,26 @@ const HeaderDesk = ({ user }) => {
         </Box>
 
         <Box display="flex" alignItems="center" gap={3}>
-          {navItems.map((item, index) => (
-            <Link to={item.to} key={index} title={item.label}>
-              <IconButton
-                sx={{
-                  color: "#444",
-                  "&:hover": {
-                    color: "#1976d2",
-                    transform: "scale(1.1)",
-                    transition: "transform 0.3s ease, color 0.3s",
-                  },
-                }}
-              >
-                {item.icon}
-              </IconButton>
-            </Link>
-          ))}
+          {navItems.map((item, index) => {
+            const isActive = location.pathname === item.to; // Verifica se o item está ativo
+            return (
+              <Link to={item.to} key={index} title={item.label}>
+                <IconButton
+                  sx={{
+                    color: isActive ? "#1976d2" : "#444", // Destaque na cor ativa
+                    backgroundColor: isActive ? "#e3f2fd" : "transparent", // Fundo ativo
+                    "&:hover": {
+                      color: "#1976d2",
+                      transform: "scale(1.1)",
+                      transition: "transform 0.3s ease, color 0.3s",
+                    },
+                  }}
+                >
+                  {item.icon}
+                </IconButton>
+              </Link>
+            );
+          })}
 
           {publicPanel && (
             <Button
@@ -76,7 +76,8 @@ const HeaderDesk = ({ user }) => {
                 "&:hover": { backgroundColor: "#1565c0" },
                 padding: "6px 12px",
                 fontWeight: "bold",
-              }}>
+              }}
+            >
               Ir para Painel Público
             </Button>
           )}
