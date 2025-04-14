e48a707f1f97131e32bd961f99b73e0966b9685e
mohvi <167302439+connectionmz@users.noreply.github.com>
2025-04-15 00:21:07 +0100

    Analistic
    Analistic

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { SaveLogError } from './utils/SaveLogError';
 import DesktopRoutes from './components/routes/DesktopRoutes';
 import NonSubscriberRoutesDesktop from './components/routes/NonSubscriberRoutesDesktop';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   const [userData, setUserData] = useState(null);
@@ -107,6 +108,7 @@ const App = () => {
     <Router>
       <div className="App">
         <div className="content">
+            <Analytics/>
             <DesktopRoutes user={userData} /> 
         </div>
       </div>
